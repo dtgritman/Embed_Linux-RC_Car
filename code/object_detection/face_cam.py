d31acc280244687b5b48cc858f09a736934a1d86
faizjamil <faizan34@gmail.com>
2019-05-12 22:04:41 -0400

    print coordinates of center of upper body

--- code/object_detection/face_cam.py
+++ code/object_detection/face_cam.py
@@ -18,17 +18,22 @@ print(cv2.__version__)
 #vs = PiVideoStream().start()
 time.sleep(2.0)
 while True:
+    # captures frames from camera, stores them in stream
     cam.capture(stream, format='jpeg', use_video_port=True)
     frame = np.fromstring(stream.getvalue(), dtype=np.uint8)
     stream.seek(0)
     #frame = vs.read()
     frame = cv2.imdecode(frame, 1)
+    # convert to grayscale for cascade detection
     gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     #hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     shirts = shirt_cascade.detectMultiScale(gray, 1.02, 5)
     #color_frame = frame
+    # draws rectangle around any objects detected
     for (x,y,w,h) in shirts:
+        # rectangle is half the height of body detected
         cv2.rectangle(frame,(x,y),(x+w,y+(h/2)),(255,255,0),2)
+        print("Coordinates of center are x:" + (x + w/2) + " y: " + (y + h/2))
     #    color_frame = frame[y:y+h, x:x+w]
     frame = cv2.resize(frame,(640,480))
     # loop over the boundaries
@@ -44,6 +49,7 @@ while True:
 
     # show the images
     #cv2.imshow('images',  output)
+    # frames are outputed
     cv2.imshow('Video', frame)
     #cv2.imshow('color frame', color_frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):

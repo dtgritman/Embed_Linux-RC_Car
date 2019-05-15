5f7f043e0d5215688ab251ba49d8697b826685f2
Dustin Gritman <dtgritman@gmail.com>
2019-05-15 02:58:18 -0400

    Added car control support

--- code/tankps4.py
+++ code/tankps4.py
@@ -37,6 +37,7 @@ def tank(tank):
                 if event.type == ecodes.EV_KEY and event.code == BTN.option and event.value == 1:
                     paused = False
                     cannon.activate()
+                    car.activate()
                 continue
             
             if event.type == ecodes.EV_KEY:
@@ -45,16 +46,14 @@ def tank(tank):
                         paused = True
                         print("Input Paused!")
                         cannon.deactivate()
+                        car.deactivate()
                         continue
-                elif event.code == BTN.x:
-                    if event.value == 1:
-                        cannonState = not cannonState
-                        tank.fireCannon(cannonState)
-                
                 elif event.code == TRIG.l1:
                     curL1Trig = event.value
                 elif event.code == TRIG.r1:
-                    curR1Trig = event.value
+                    if event.value == 1:
+                        cannonState = not cannonState
+                        tank.fireCannon(cannonState)
             
             elif event.type == ecodes.EV_ABS:
                 #Range is 0 to 255, Center values are about 127 (up/left: below 127, down/right: above 127)
@@ -68,25 +67,23 @@ def tank(tank):
                 elif event.code == JS.right_y:
                     curRJs["y"] = event.value
             
-            drivePos = 0
-            if curL1Trig and not curR1Trig:
-                drivePos = -1
-            elif curR1Trig and not curL1Trig:
-                drivePos = 1
-            
             steerPos = 0
-            if curLJs.get("x") < 121:
-                # TODO: Edit this for proper steering control
-                steerPos = -(122 - curLJs.get("x"))
-            elif curLJs.get("x") > 131:
-                # TODO: Edit this for proper steering control
-                steerPos = curLJs.get("x") - 133
+            if curLJs.get("x") > 160:
+                steerPos = 1
+            elif curLJs.get("x") < 90:
+                steerPos = -1
+                
+            drivePos = 0
+            if curLJs.get("y") > 160:
+                drivePos = 1
+            elif curLJs.get("y") < 90:
+                drivePos = -1
             
-            servoPos = 0
+            basePos = 0
             if curRJs.get("x") < 121:
-                servoPos = (-45 / 122) * (122 - curRJs.get("x"))
+                basePos = (-45 / 122) * (122 - curRJs.get("x"))
             elif curRJs.get("x") > 133:
-               servoPos = (45 / 122) * (curRJs.get("x") - 133)
+               basePos = (45 / 122) * (curRJs.get("x") - 133)
             
             stepperPos = 0
             if curRJs.get("y") < 121:
@@ -96,9 +93,9 @@ def tank(tank):
             
             
             # TODO: Add car drive and steering controls
-            tank.setBaseRotation(int(servoPos))
+            tank.setBaseRotation(int(basePos))
             tank.setCannonAngle(int(stepperPos))
-            print("CannonState: {}, ServoPos: {}, StepperPos: {}, DrivePos: {}, SteerPos: {}".format(cannonState, int(servoPos), int(stepperPos), drivePos, steerPos))
+            print("CannonState: {}, BasePos: {}, StepperPos: {}, DrivePos: {}, SteerPos: {}".format(cannonState, int(basePos), int(stepperPos), drivePos, steerPos))
         
     except OSError:
         print("{} - Controller Disconnected!".format(time.strftime("%H:%M:%S")))
@@ -116,13 +113,22 @@ AIN2 = 5
 AIN1 = 6
 BIN1 = 19
 BIN2 = 26
-
 cannon = TankCannon(pinCannon, pinServo, StepperMotor(AIN1, AIN2, BIN1, BIN2), 3)
+
+carSTBY = 27
+carPWMA = 4
+carAIN2 = 17
+carAIN1 = 18
+carBIN1 = 22
+carBIN2 = 23
+carPWMB = 24
+car = Car(carSTBY, carPWMA, carAIN2, carAIN1, carBIN1, carBIN2, carPWMB)
 try:
     # run cannon while controller connected
     while tank(cannon):
         # if controller disconnected: stop tank
         cannon.deactivate()
+        car.deactivate()
         time.sleep(5)
 
 except KeyboardInterrupt:
